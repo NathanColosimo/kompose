bf8cc5f08d7acbd3df9b63852b2b934033a7de40
nathancolosimo <nathancolosimo@gmail.com>
2025-11-23 13:26:42 -0500

    Typed db client

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,7 @@
+/** biome-ignore-all lint/performance/noNamespaceImport: Drizzle schema */
 import { env } from "@kompose/env";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as authSchema from "./schema/auth";
+import * as taskSchema from "./schema/task";
 
-export const db = drizzle(env.DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL, { schema: { ...authSchema, ...taskSchema } });
