3d3b1f311d696e2838e2169c697fc2cd2357f5a6
nathancolosimo <nathancolosimo@gmail.com>
2025-12-04 16:55:53 -0500

    Removed prepared statements

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,9 @@
 /** biome-ignore-all lint/performance/noNamespaceImport: Drizzle schema */
 import { env } from "@kompose/env";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as authSchema from "./schema/auth";
 import * as taskSchema from "./schema/task";
 
-export const db = drizzle(env.DATABASE_URL, {
-  schema: { ...authSchema, ...taskSchema },
-});
+const client = postgres(env.DATABASE_URL, { prepare: false });
+export const db = drizzle(client, { schema: { ...authSchema, ...taskSchema } });
