27440c956b10a89ab13b92510a72128475e27766
nathancolosimo <nathancolosimo@gmail.com>
2026-02-04 11:07:53 -0500

    use bun sql driver

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,14 @@
 /** biome-ignore-all lint/performance/noNamespaceImport: Drizzle schema */
 import { env } from "@kompose/env";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { SQL } from "bun";
+import { drizzle } from "drizzle-orm/bun-sql";
 import * as authSchema from "./schema/auth";
 import * as relationsSchema from "./schema/relations";
 import * as tagSchema from "./schema/tag";
 import * as taskSchema from "./schema/task";
 
-const client = postgres(env.DATABASE_URL, { prepare: false });
+const client = new SQL(env.DATABASE_URL);
+
 export const db = drizzle(client, {
   schema: { ...authSchema, ...taskSchema, ...tagSchema, ...relationsSchema },
 });
